1ecffe89bdbdb7150e6ff31683fa7caefc3f062c
Madeline Kehl <mad.kehl@gmail.com>
2020-10-11 14:29:52 -0500

    feat: initial commit

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# Auto2Cluster
+# ProzhitoAutoencoder2Cluster
+
+Description:  The following scripts are intended to allow the user to 1) select key root words in Russian, 2) search the Prozhito database (collection of Russian diaries from 1600-modern day) for instances of those words and pull the word vector representations of those words, 3) compress the word vectors using an autoencoder and 4) cluster the final product.   
+
+# Installation
+        
+Notes: currently this repository is insufficient to run the first half of the code (selecting entries based on key stems and vectorizing them) It is missing both a) the sql connection/permissions to access the full database b) the russian language word vector model and c) several large files exceeding the 100 MB specification. Making these available for public use is still a work in progress.
+
+# Files included:
+* **keyword_vecs (will not work with current repository resources)**: this extracts relevant sentences and convert to word vectors
+* **autoencoder_stikhi**:  this is an autoencoder set up used to compress the vector, written as a sort of psuedo function.  I found it easier to adjust the various parameters in this way as opposed to using a traditional function.
+* **hdbscan_clusterer**:  this clusters the compressed vectors.  Since the HDBSCAN algorithm marks various points as noise, there is also an option to fit noise points retroactively with both a svm and rf classifier.  These are rather crude at the moment and do not have grid search etc built in to them.  Additionally, it is suggested to examine the clusters with and without noise, to make sure that fitting the noise points post hoc is a sensible thing to do
+* **090120_autoencoderhelperfunctions**:  contains many relevant functions, including the one to run HDBSCAN + an ML classifier
+
+# Current Requirements:
+pandas, numpy, nltk, math, gensim, punctuation, Word2Vec(from gensim.models), sqlalchemy, punctuation(from string), sub (from re), tensorflow, keras, spatial(from scipy), HDBSCAN, sklearn , plotly, hiplot(optional), matplotlib
+
+
+
+# MIT License
+
+Copyright (c) 2020 Madeline Kehl
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
---
+++ autoencoderhelperfunctions.py
@@ -0,0 +1,228 @@
+#basic imports
+import pandas as pd
+import numpy as np
+import math
+#ml stuff
+import tensorflow as tf
+from tensorflow import keras
+from keras.layers import Dense
+from keras.models import Model
+from keras.models import Sequential
+#metrics + sklearn
+from scipy import spatial
+from hdbscan import HDBSCAN
+from sklearn import metrics
+from sklearn import decomposition
+from sklearn import svm
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+#data visualization
+import plotly.graph_objs as go
+import plotly
+import hiplot as hip
+
+#this function can extract any named layer from a given model
+#takes str, keras model, original dataframe
+def layer_extract(layer_name, mod, v_dat):
+    layer_name = layer_name
+    intermediate_layer_model = Model(inputs=mod.input,
+                                 outputs=mod.get_layer(layer_name).output)
+    intermediate_output = intermediate_layer_model.predict(v_dat)
+    es = intermediate_output
+    
+    yp = es[0]
+    for n in es[1:len(es)]:
+        yp = np.vstack((yp, n))
+        
+    return(yp)
+
+#this function will return a list of cosine distances between matched rows of two given dataframes
+#takes two data frames
+def get_cos(dft, dfp):
+    cos_val = []
+    for i, j in enumerate(dft):
+        p = dfp[i]
+        dist = spatial.distance.cosine(j, p)
+        if math.isnan(dist) == False:
+            cos_val.append(1 - dist)
+            
+    return(cos_val)
+
+#this saves a graph illustrating points within a 3 dimensional space (if input is more than 3 dim, takes first 3)
+#takes data, labels for data, color scheme, can specify color scale or name if desired
+def make_graph(X1, hovert, marking, colors = 'Inferno', name = '150_80_30_8np_712tf.html', PCA = False):
+    
+    if PCA == True:
+        pca = decomposition.PCA(n_components= 3, random_state = 12)
+        pca.fit(X1)
+        X1 = pca.transform(X1)
+    
+    x = X1[:,0]
+    y = X1[:,1]
+    z = X1[:,2]
+
+
+    fig = go.Figure(data=[go.Scatter3d(
+        x=x,
+        y=y,
+        z=z,
+        hovertext = hovert,
+        mode='markers+text',
+        marker=dict(
+            size=3,
+       # color =  n,
+            color =  marking,                # set color to an array/list of desired values
+            colorscale=colors,   # choose a colorscale
+            opacity=0.8,
+            showscale = True
+        )
+    )])
+
+
+# tight layout
+    fig.update_layout(width = 1000, height = 500, margin=dict(l=0, r=0, b=0, t=0))
+
+    plotly.offline.plot(fig, filename= name)
+    
+    
+def make_graph2d(X1, hovert, marking, colors = 'Inferno', name = '150_80_30_8np_712tf.html', PCA = False):
+    
+    if PCA == True:
+        pca = decomposition.PCA(n_components= 2, random_state = 12)
+        pca.fit(X1)
+        X1 = pca.transform(X1)
+    
+    x = X1[:,0]
+    y = X1[:,1]
+
+
+
+    fig = go.Figure(data=[go.Scatter(
+        x=x,
+        y=y,
+        hovertext = hovert,
+        mode='markers+text',
+        marker=dict(
+            size=3,
+       # color =  n,
+            color =  marking,                # set color to an array/list of desired values
+            colorscale=colors,   # choose a colorscale
+            opacity=0.8,
+            showscale = True
+        )
+    )])
+
+
+# tight layout
+    fig.update_layout(width = 1000, height = 500, margin=dict(l=0, r=0, b=0, t=0))
+
+    plotly.offline.plot(fig, filename= name)
+
+# this function does not actually create a hiplot object, but converts keras history into a list of dictionaries interperable
+#by hiplot.  takes the history object and a series of relevant labels
+def create_hiplot(hist_ob, activ, lr,CS, CSR, encoder, KI, lf):
+    hist_df = pd.DataFrame(hist_ob.history)
+    hist = hist_df.to_dict('records')
+    trial_num = 0
+    for i in hist:
+        dict1 = {'activation': activ, 'lr': lr,'CS': CS, 'CSR': CSR, 'encoder': encoder, 'KI': KI, 'lossfxn': lf}
+        i = i.update(dict1)
+    return(hist)
+
+#this allows you to add multiple history objects to the input for hiplot (desirable for comparing models)       
+def update_hiplot(hipl, hist_ob, activ, lr,encoder, KI, lf,CS = None, CSR = None):
+    hist_df = pd.DataFrame(hist_ob.history)
+    hist = hist_df.to_dict('records')
+    trial_num = 0
+    for i in hist:
+        dict1 = {'activation': activ, 'lr': lr,'CS': CS, 'CSR': CSR, 'encoder': encoder, 'KI': KI, 'lossfxn': lf}
+        i = i.update(dict1)
+    return(hipl + hist)
+
+#this allows you to remove content from specific iterations 
+def delete_from_hiplot(hip1, activ, encoder):
+    hi2 = []
+    for i in hip1:
+        if i['activation'] != activ or i['encoder'] != encoder:
+            hi2.append(i)
+    return(hi2)
+
+def return_hdbscansvm(df, txt_col, rf = False, clust_size = 15,  samp_size = 5, svmx = False, svmc = 100, clust_metric = 'braycurtis'):
+
+    super_flat =pd.DataFrame(df)
+    r = super_flat
+    
+    hdb = HDBSCAN(min_cluster_size = clust_size, min_samples= samp_size, metric = clust_metric, cluster_selection_method = 'leaf')
+    n = hdb.fit_predict(r)
+    f = pd.get_dummies(n)
+
+    with_cat = pd.Series([str(i) for i in list(zip(txt_col, n))], name = 'text')
+    answers = pd.concat([with_cat, f], axis = 1 )
+    
+    answers = [answers, n, hdb]
+    if svmx == True:
+
+        ans_ = pd.concat([pd.DataFrame(txt_col), f], axis = 1 )
+        ans_ = ans_[ans_[-1] == 0]
+        ans_ = ans_.drop(-1, axis = 1)
+        ans_ = ans_.melt(id_vars='text', var_name='cluster3', value_name='value')
+        ans_ = ans_[ans_['value'] == 1]
+        ans_ = ans_.drop('value', axis = 1)
+        ans_svm = ans_[ans_['cluster3'] > -1]
+        for_x = pd.concat([pd.DataFrame(txt_col),pd.DataFrame(r)], axis = 1)
+        authorssvm = for_x.merge(ans_svm,left_on = 'text', right_on= 'text', how = 'right')
+        y = list(authorssvm['cluster3'])
+        X = authorssvm.drop(['cluster3', 'text'], axis = 1)
+        X = X.fillna(0)
+
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5)
+        clf = svm.SVC(C = svmc, kernel = 'rbf', gamma = 0.7, random_state = 12)
+        print('done')
+
+        clf.fit(X_train,y_train)
+        print('done')
+        y_pred= clf.predict(X_test)
+
+        print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
+
+
+        nsvm = clf.predict(r)
+        f = pd.get_dummies(nsvm)
+
+        with_catsvm = pd.Series([str(i) for i in list(zip(txt_col, nsvm))], name = 'text')
+        answerssvm = pd.concat([with_catsvm, f], axis = 1 )
+        answers = [answerssvm, nsvm, hdb]
+    if rf == True:
+
+        ans_ = pd.concat([pd.DataFrame(txt_col), f], axis = 1 )
+        ans_ = ans_[ans_[-1] == 0]
+        ans_ = ans_.drop(-1, axis = 1)
+        ans_ = ans_.melt(id_vars='text', var_name='cluster3', value_name='value')
+        ans_ = ans_[ans_['value'] == 1]
+        ans_ = ans_.drop('value', axis = 1)
+        ans_svm = ans_[ans_['cluster3'] > -1]
+        for_x = pd.concat([pd.DataFrame(txt_col),pd.DataFrame(r)], axis = 1)
+        authorssvm = for_x.merge(ans_svm,left_on = 'text', right_on= 'text', how = 'right')
+        y = list(authorssvm['cluster3'])
+        X = authorssvm.drop(['cluster3', 'text'], axis = 1)
+        X = X.fillna(0)
+
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+        clf = RandomForestClassifier(min_samples_split =2,max_features = 3, max_depth = 10000, random_state=12)
+        print('done')
+
+        clf.fit(X_train,y_train)
+        print('done')
+        y_pred= clf.predict(X_test)
+
+        print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
+
+
+        nsvm = clf.predict(r)
+        f = pd.get_dummies(nsvm)
+
+        with_catsvm = pd.Series([str(i) for i in list(zip(txt_col, nsvm))], name = 'text')
+        answerssvm = pd.concat([with_catsvm, f], axis = 1 )
+        answers = [answerssvm, nsvm, hdb]
+    
+    return(answers)
